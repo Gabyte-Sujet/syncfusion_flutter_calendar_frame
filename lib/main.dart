13c37a4d751aedcc28d77b331c152d8b7a0a106d
Gabyte-Sujet <vakogabaidze@gmail.com>
2022-10-27 15:12:49 +0400

    changed something

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   CalendarController _calendarController = CalendarController();
-  late DateTime today;
+  late DateTime today = DateTime.now();
 
   // void tst2() {
   //   _calendarController.displayDate = DateTime(2022, 10, 28);
@@ -103,28 +103,46 @@ class _HomePageState extends State<HomePage> {
                   _calendarController.displayDate = daysOfMonth[index]['tst2'];
                   today = daysOfMonth[index]['tst2'];
                   tst();
+                  setState(() {});
                   print(daysOfMonth[index]['tst2']);
                 },
-                child: Container(
-                  height: 70,
-                  width: 40,
-                  margin: EdgeInsets.only(right: 10),
-                  decoration: BoxDecoration(
-                    color: Color(0xffB01E76),
-                    borderRadius: BorderRadius.circular(28),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        daysOfMonth[index]['weekDay'],
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        daysOfMonth[index]['day'].toString(),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 2),
+                  child: Container(
+                    height: 70,
+                    width: 40,
+                    margin: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      color: today.day == daysOfMonth[index]['day']
+                          ? Color(0xffB01E76)
+                          : Color(0xff27A6B0),
+                      boxShadow: [
+                        if (today.day == daysOfMonth[index]['day'])
+                          BoxShadow(
+                            color: Color(0xffB01E76),
+                            blurRadius: 3,
+                          ),
+                        if (today.day != daysOfMonth[index]['day'])
+                          BoxShadow(
+                            color: Color(0xff27A6B0),
+                            blurRadius: 3,
+                          )
+                      ],
+                      borderRadius: BorderRadius.circular(28),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          daysOfMonth[index]['weekDay'],
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          daysOfMonth[index]['day'].toString(),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -139,10 +157,10 @@ class _HomePageState extends State<HomePage> {
                 // initialDisplayDate: DateTime.now().add(Duration(days: 1)),
                 view: CalendarView.month,
                 allowViewNavigation: true,
-                allowedViews: <CalendarView>[
-                  CalendarView.day,
-                  CalendarView.month,
-                ],
+                // allowedViews: <CalendarView>[
+                //   CalendarView.day,
+                //   CalendarView.month,
+                // ],
 
                 showNavigationArrow: true,
                 firstDayOfWeek: 1,
@@ -150,9 +168,31 @@ class _HomePageState extends State<HomePage> {
 
                 headerHeight:
                     _calendarController.view == CalendarView.day ? 0 : 40,
-                // viewHeaderHeight: 0,
+                viewHeaderHeight:
+                    _calendarController.view == CalendarView.day ? 0 : -1,
 
-                // onTap: (d) => print(d.resource),
+                // ------- CalendarView.month an day-t gansazgvrav klikze ra unda moxdes. if day ...
+                // --- d.appointments == null -it vamocmebt(ganvsazgvravt) sxvanairad ar gamodis,
+                onTap: (d) {
+                  // if (_calendarController.view == CalendarView.day) {
+                  //   print('day');
+                  //   showModalBottomSheet(
+                  //       context: context,
+                  //       builder: (context) => AlertDialog(
+                  //             content: Text('asdasd'),
+                  //           ));
+                  // } else {
+                  //   print('month');
+                  // }
+                  if (d.appointments == null) {
+                    print('day');
+                  } else {
+                    print('month');
+                  }
+                  setState(() {});
+                  print('tap: ${d.appointments}');
+                  print('tap: ${d.targetElement}');
+                },
 
                 headerStyle: CalendarHeaderStyle(
                   textAlign: TextAlign.center,
@@ -162,10 +202,15 @@ class _HomePageState extends State<HomePage> {
                     color: Color(0xff8F9BB3),
                   ),
                 ),
+
                 onSelectionChanged: (d) {
-                  print(d.date);
-                  today = d.date!;
-                  tst();
+                  // ------------- aq atrakebs roca javshans daacvebi javshnis events ?????? !!!!!!
+                  if (d.date != null) {
+                    print(d.date);
+                    today = d.date!;
+                    tst();
+                  }
+
                   // setState(() {});
                 },
                 appointmentBuilder: (context, details) {
@@ -201,8 +246,17 @@ class _HomePageState extends State<HomePage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
+                            Text(
+                              detail.patientName,
+                              style: TextStyle(
+                                color: Color(0xffFC45B2),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            SizedBox(height: 15),
                             Text(detail.eventName),
-                            // Spacer(),
+                            Spacer(),
                             Row(
                               children: [
                                 Icon(Icons.timelapse),
@@ -217,6 +271,7 @@ class _HomePageState extends State<HomePage> {
                     ],
                   );
                 },
+                showCurrentTimeIndicator: false,
                 timeSlotViewSettings: const TimeSlotViewSettings(
                   // dateFormat: '',
                   // dayFormat: '',
@@ -224,10 +279,27 @@ class _HomePageState extends State<HomePage> {
                   startHour: 9,
                   endHour: 22,
                   timeInterval: Duration(minutes: 15),
-                  timeFormat: 'hh:mm',
-                  timeRulerSize: 50,
+                  timeFormat: 'HH:mm',
+                  timeRulerSize: 70,
+                  timeTextStyle: TextStyle(
+                    color: Color(0xff78818F),
+                    fontSize: 14,
+                  ),
+                  timeIntervalHeight: 50,
                 ),
-
+                // cellBorderColor: Colors.transparent,
+                timeRegionBuilder: (BuildContext context,
+                    TimeRegionDetails timeRegionDetails) {
+                  print(timeRegionDetails.region.startTime);
+                  return Container(
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Icon(
+                      Icons.restaurant,
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                  );
+                },
                 monthCellBuilder: (context, details) {
                   // if today == datetime.now -->> mashin .....
                   var today = DateTime.now();
@@ -288,6 +360,8 @@ class _HomePageState extends State<HomePage> {
                     ],
                   );
                 },
+                // cellBorderColor: Colors.transparent,
+
                 monthViewSettings: const MonthViewSettings(
                   appointmentDisplayMode: MonthAppointmentDisplayMode.none,
                 ),
@@ -305,12 +379,25 @@ class _HomePageState extends State<HomePage> {
   List<Meeting> _getDataSource() {
     final List<Meeting> meetings = <Meeting>[];
     final DateTime today = DateTime.now();
+    // final DateTime yesterday = DateTime.now().subtract(Duration(days: 1));
     final DateTime startTime =
         DateTime(today.year, today.month, today.day, 9, 0, 0);
     final DateTime endTime = startTime.add(const Duration(hours: 2));
     meetings.add(
-      Meeting('Conference', startTime, endTime, const Color(0xFF0F8644), false),
+      Meeting('პაციენტის სახელი', 'კბილის დაპლომბვა', startTime, endTime,
+          const Color(0xFF0F8644), false),
     );
+
+    // meetings.add(
+    //   Meeting('Conference', startTime, endTime, const Color(0xFF0F8644), false),
+    // );
+
+    // meetings.add(
+    //   Meeting('Conference', yesterday, endTime, const Color(0xFF0F8644), false),
+    // );
+    // meetings.add(
+    //   Meeting('Conference', yesterday, endTime, const Color(0xFF0F8644), false),
+    // );
 
     return meetings;
   }
@@ -348,8 +435,10 @@ class MeetingDataSource extends CalendarDataSource {
 }
 
 class Meeting {
-  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+  Meeting(this.patientName, this.eventName, this.from, this.to, this.background,
+      this.isAllDay);
 
+  String patientName;
   String eventName;
   DateTime from;
   DateTime to;
